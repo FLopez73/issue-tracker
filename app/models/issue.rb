f30df6bf93c8b3fb921345bb85d52cabe964e120
flopez73 <flopez730@gmail.com>
2023-12-06 14:08:20 -0800

    validations

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,8 @@
 class Issue < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :priority, presence: true
+    validates :assignee, presence: true
+    validates :status, presence: true
 end
